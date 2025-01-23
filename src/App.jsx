6e7ced81956ddaf7b438d60d2662fa6294bf0a2f
rubi-dahal <rrubidahal7@gmail.com>
2025-01-23 15:27:59 +0545

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+import LoginSignup from './components/Login-signup/LoginSignup'
+const App = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  )
+}
+
+export default App
